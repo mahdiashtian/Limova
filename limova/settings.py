09793ca3ed26cf6eed0d4b9fb0b9562d7a66ed54
Mahdi <74005352+mahdiashtian@users.noreply.github.com>
2024-08-13 21:26:20 +0330

    Update settings.py

--- limova/settings.py
+++ limova/settings.py
@@ -134,13 +134,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 
-STATIC_URL = 'static/'
-
-STATIC_ROOT = os.path.join(BASE_DIR, "static_cdn", "static_root")
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
-
-MEDIA_ROOT = os.path.join(BASE_DIR, "static_cdn", "media_root")
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "assets")
