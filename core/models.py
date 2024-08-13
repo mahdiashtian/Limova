8440a38eee61ee3e99fcfa80de9ba0db5989f9e6
Mahdi <74005352+mahdiashtian@users.noreply.github.com>
2024-08-13 18:17:29 +0330

    Create core app

---
+++ core/models.py
@@ -0,0 +1,24 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+from django.utils import timezone
+
+User = get_user_model()
+
+
+class BaseModel(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='سازنده')
+    created_at = models.DateTimeField(default=timezone.now, verbose_name='تاریخ ایجاد')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='تاریخ اپدیت')
+    archived_at = models.DateTimeField(null=True, blank=True, editable=False, verbose_name='تاریخ حذف')
+
+    class Meta:
+        abstract = True
+
+
+class Media(BaseModel):
+    owner = None
+    media = models.FileField()
+
+    class Meta:
+        verbose_name = 'رسانه'
+        verbose_name_plural = 'رسانه ها'
