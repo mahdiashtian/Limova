82b01ba7051d79db005cb25f3f9f9d6209df96fd
Mahdi <74005352+mahdiashtian@users.noreply.github.com>
2024-09-05 14:56:43 +0330

    Add user urls to limova url

--- limova/urls.py
+++ limova/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls', namespace='core')),
+    path('', include('users.urls', namespace='users')),
 ]
---
+++ users/urls.py
@@ -0,0 +1,9 @@
+# urls.py
+from django.urls import path
+
+from users.views import LoginView
+
+app_name = 'users'
+urlpatterns = [
+    path("login", LoginView.as_view(), name="login"),
+]
