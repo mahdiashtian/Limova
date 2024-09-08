d8d52210e848965770427c5f809cf0ac9849ddff
Mahdi <74005352+mahdiashtian@users.noreply.github.com>
2024-09-08 17:29:13 +0330

    Add about us to model and change media field

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from core.models import Media, ContactUs
+from core.models import Media, ContactUs, AboutUs
 
 admin.site.register(Media)
 admin.site.register(ContactUs)
+admin.site.register(AboutUs)
--- core/models.py
+++ core/models.py
@@ -2,6 +2,8 @@ from django.contrib.auth import get_user_model
 from django.db import models
 from django.utils import timezone
 
+from core.choices import TypeChoices
+
 User = get_user_model()
 
 
@@ -18,10 +20,15 @@ class BaseModel(models.Model):
 class Media(BaseModel):
     owner = None
     media = models.FileField()
+    type = models.CharField(max_length=30, choices=TypeChoices.choices, default=TypeChoices.OTHER)
+    name = models.CharField(null=True, blank=True)
 
     class Meta:
         verbose_name = 'رسانه'
         verbose_name_plural = 'رسانه ها'
+
+    def __str__(self):
+        return f"{self.id}-{self.type}-{self.name}"
 
 
 class ContactUs(BaseModel):
@@ -34,3 +41,43 @@ class ContactUs(BaseModel):
     class Meta:
         verbose_name = 'تماس'
         verbose_name_plural = 'تماس ها'
+
+
+class AboutUs(BaseModel):
+    owner = None
+    image = models.ForeignKey(Media, on_delete=models.CASCADE, verbose_name="تصویر", related_name='about_us_image')
+    # thumbnail = models.ForeignKey(Media, on_delete=models.CASCADE, verbose_name="تصویر کوچک",
+    #                               related_name='about_us_thumbnail', null=True, blank=True)
+    title_1 = models.CharField(max_length=255, verbose_name="عنوان یک")
+    under_title_1 = models.TextField(verbose_name="زیر عنوان یک")
+    title_2 = models.CharField(max_length=255, verbose_name="عنوان دو")
+    under_title_2 = models.TextField(verbose_name="زیر عنوان دو")
+    title_3 = models.CharField(max_length=255, verbose_name="عنوان سه")
+    under_title_3 = models.TextField(verbose_name="زیر عنوان سه")
+    gallery = models.ManyToManyField(Media, related_name='about_us_gallery', blank=True)
+
+    # def clean(self):
+    #     super().clean()
+    #     photo = self.image.media
+    #     if photo:
+    #         img = Image.open(photo)
+    #         width, height = img.size
+    #         if width < 566 or height < 478:
+    #             raise ValidationError(f"Image must be at least 566x478 pixels. Current size: {width}x{height}.")
+
+    # def save(self, *args, **kwargs):
+    #     self.full_clean()
+    #     thumbnail = self.thumbnail
+    #     if thumbnail:
+    #         thumbnail_media = thumbnail.media
+    #         if os.path.isfile(thumbnail_media.path):
+    #             os.remove(thumbnail_media.path)
+    #     self.thumbnail = Media.objects.create(media=create_thumbnail(self.image.media), type=TypeChoices.THUMBNAIL)
+    #
+    #     super().save(*args, **kwargs)
+    #     if thumbnail:
+    #         thumbnail.delete()
+
+    class Meta:
+        verbose_name = 'درباره ما'
+        verbose_name_plural = 'درباره ما'
