42586a99a9e1a2fab17fcfdb15754c781047c142
Mahdi <74005352+mahdiashtian@users.noreply.github.com>
2024-09-08 18:34:09 +0330

    Update admin.py

--- users/admin.py
+++ users/admin.py
@@ -3,10 +3,28 @@ from django.contrib import admin
 from users.models import User
 from django.contrib.auth.models import Group, Permission
 from django.contrib.auth.admin import (UserAdmin as BaseUserAdmin, GroupAdmin as BaseGroupAdmin)
+from django.utils.translation import gettext_lazy as _
 
 
 class UserAdmin(BaseUserAdmin):
-    pass
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "email", "avatar")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                    "rule",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
 
 
 class PermissionAdmin(admin.ModelAdmin):
