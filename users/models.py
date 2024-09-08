feaf47e4c64f5c82e196919bad6d2da3c593dec7
Mahdi <74005352+mahdiashtian@users.noreply.github.com>
2024-09-08 17:30:06 +0330

    Add avatar and rule field to model

--- users/models.py
+++ users/models.py
@@ -1,5 +1,10 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+from core.choices import RuleChoices
 
 
 class User(AbstractUser):
-    ...
+    avatar = models.ForeignKey('core.Media', null=True, blank=True, verbose_name="تصویر پروفایل",
+                               on_delete=models.SET_NULL)
+    rule = models.CharField(max_length=50, choices=RuleChoices.choices, default=RuleChoices.DEVELOPER)
