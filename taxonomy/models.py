205e454e0b51d771d4da92801ddb3a6426bfd231
Mahdi <74005352+mahdiashtian@users.noreply.github.com>
2024-09-12 15:28:17 +0330

    Update comment model

--- taxonomy/models.py
+++ taxonomy/models.py
@@ -1,6 +1,12 @@
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
+from articles.models import Article
+from core.choices import CommentTypeChoices
 from core.models import BaseModel
+from products.models import Product
 
 
 class Tag(models.Model):
@@ -27,8 +33,30 @@ class Category(models.Model):
 
 class Comment(BaseModel):
     text = models.CharField(max_length=255, verbose_name="متن کامنت")
-    rate = models.IntegerField(verbose_name="امتیاز")
+    rate = models.IntegerField(verbose_name="امتیاز", validators=[MinValueValidator(0), MaxValueValidator(5)])
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, editable=False)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey("content_type", "object_id")
+    type = models.CharField(choices=CommentTypeChoices.choices, default=CommentTypeChoices.PRODUCT)
+
+    def save(
+            self,
+            *args,
+            force_insert=False,
+            force_update=False,
+            using=None,
+            update_fields=None,
+    ):
+        if self.type == CommentTypeChoices.PRODUCT:
+            self.content_type = ContentType.objects.get_for_model(Product)
+        elif self.type == CommentTypeChoices.BLOG:
+            self.content_type = ContentType.objects.get_for_model(Article)
+        return super().save(*args, force_insert=force_insert, force_update=force_update, using=using,
+                            update_fields=update_fields)
 
     class Meta:
         verbose_name = 'کامنت'
         verbose_name_plural = 'کامنت ها'
+        indexes = [
+            models.Index(fields=["content_type", "object_id", "type"]),
+        ]
