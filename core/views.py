55a81ca7bfa68cf61fe97fa4c1784127dbeed611
Mahdi <74005352+mahdiashtian@users.noreply.github.com>
2024-09-12 16:44:48 +0330

    Add gallery limova

--- core/views.py
+++ core/views.py
@@ -16,6 +16,8 @@ class HomeView(TemplateView):
     def get_context_data(self, **kwargs):
         data = super().get_context_data(**kwargs)
         data['sliders'] = Slider.objects.all().filter(is_active=True).order_by('created_at')[:3]
+        data['comments'] = Comment.objects.all().filter(rate__range=[4, 5])[:2]
+        data['obj'] = AboutUs.objects.first()
         return data
 
 
